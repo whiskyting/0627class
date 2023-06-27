c2e217c971c9c9dfe8f83c7520108bf38b3d2890
User <User@WS7.r1802.tciii.org.tw>
2023-06-27 09:08:15 +0800

    123

---
+++ BradJava/src/tw/org/iii/twtor/Brad01.java
@@ -0,0 +1,32 @@
+package tw.org.iii.twtor;
+
+public class Brad01 {
+	/*
+	 * Brad 01 OK
+	 */
+	//註解
+	/**
+	 *
+	 * @param args
+	 */
+	// 產生JAVA DOC 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// (TODO頁籤表示未寫完)
+		// 紅色為JAVA關鍵字
+		// 開發者定義為黑色
+		//強型別(規定很嚴格)的程式語言：任何一個變數一開始就要指定
+		//型別->基本:整數、浮點、布林、字元
+		//   ->物件
+		byte var1;
+		var1 = 123;
+		System.out.println(var1);
+		byte var2 = -128;
+		System.out.println(var2);
+		//變數命名[a-z A-Z $_][a-z A-Z 0-9 $_]*(0或多個)
+		short var3=32767;
+		System.out.println(var3);
+
+	}
+
+}
