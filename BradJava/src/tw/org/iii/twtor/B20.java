acd609673217d1cd80b8e339b68949979862f54c
User <User@WS7.r1802.tciii.org.tw>
2023-06-29 14:11:11 +0800

    123

---
+++ BradJava/src/tw/org/iii/twtor/B20.java
@@ -0,0 +1,21 @@
+package tw.org.iii.twtor;
+
+public class B20 {
+
+	public static void main(String[] args) {
+		int a = 10, b = 3;
+		int c = a;
+		a=b;
+		b=c;
+//		a = a+b;
+//		b = a-b;
+//		a = a-b;
+		
+		
+		System.out.printf("a=%d,b=%d",a,b);
+		
+		
+
+	}
+
+}
